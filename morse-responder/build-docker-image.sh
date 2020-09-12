c253875aeec34b2c3a00143671bf4d247e372cb8
Igor Yurchenko <harry.urcen@gmail.com>
2020-09-12 14:38:22 +0300

    add tested Dockerfiles and build scripts

---
+++ morse-responder/build-docker-image.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+BASE_DIR=$( cd `dirname $0`; pwd )
+
+cd ${BASE_DIR}
+git clone https://github.com/northbear/cb-ms-morse.git
+docker image build --rm --tag morse-responder:v1 --no-cache .
